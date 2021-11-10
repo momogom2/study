8b0ee199b69c385e09765480cb8c67d70d3355c8
김재훈/전시개발팀 <momogom.kim@musinsa.com>
2021-11-10 12:55:11 +0900

    source tree test

---
+++ advanced/src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,27 @@
+package hello.advanced.app.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    public void save(String itemId) {
+
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("얘외 발생");
+        }
+        this.sleep(1000);
+    }
+
+    private void sleep(int mills) {
+
+        try {
+            Thread.sleep(mills);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
