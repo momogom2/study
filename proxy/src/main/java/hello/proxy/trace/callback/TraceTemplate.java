b3ae841c63d675761706145fcbdcd66e76b8795f
김재훈/전시개발팀 <momogom.kim@musinsa.com>
2021-12-08 17:02:00 +0900

    hw5

--- proxy/src/main/java/hello/proxy/trace/callback/TraceTemplate.java
+++ proxy/src/main/java/hello/proxy/trace/callback/TraceTemplate.java
@@ -1,7 +1,7 @@
-package hello.advanced.trace.callback;
+package hello.proxy.trace.callback;
 
-import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.logtrace.LogTrace;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
 
 public class TraceTemplate {
 
@@ -13,17 +13,17 @@ public class TraceTemplate {
 
     public <T> T execute(String message, TraceCallback<T> callback) {
         TraceStatus status = null;
-
         try {
             status = trace.begin(message);
 
+            //로직 호출
             T result = callback.call();
 
             trace.end(status);
+            return result;
         } catch (Exception e) {
             trace.exception(status, e);
             throw e;
         }
     }
-
 }
